3340e7dd2a667bfdbf9c595653297108e32897d8
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2022-02-15 11:44:21 +0000

    Create Alcohol Drinking Age.py

---
+++ Alcohol Drinking Age.py
@@ -0,0 +1,8 @@
+
+print("----Alcohol Drinking Age-----")
+print("Enter your age:  ")
+age = int(input())
+if age >= 18: 
+    print("You can drink alcohol.")
+else: 
+    print("You are too young to drink alcohol")
