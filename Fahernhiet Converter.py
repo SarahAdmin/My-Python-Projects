253dd063bb3870cb696dc57db505d595bffe9979
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2022-02-15 11:39:34 +0000

    Create Fahernhiet Converter.py

---
+++ Fahernhiet Converter.py
@@ -0,0 +1,16 @@
+def convertToFahrenheit(tc):	  		  	    	    	 	       	
+   return 9 * tc / 5 + 32	  		  	    	    	 	       	
+	  		  	    	    	 	       	
+	  		  	    	    	 	       	
+print(convertToFahrenheit(21.0),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(21.5),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(22.5),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(23.0),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(23.5),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(24.0),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(24.5),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(25.0),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(25.5),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(26.0),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(26.5),"degrees in Fahrenheit")	  		  	    	    	 	       	
+print(convertToFahrenheit(27.0),"degrees in Fahrenheit")
