1c2d30c65da98d7816f7fe1bf1f7e8e43544ccc7
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2022-02-15 11:47:44 +0000

    Create Theory Test Results.py

---
+++ Theory Test Results.py
@@ -0,0 +1,8 @@
+print("----------------Theory Test--------------------")
+name = input("Type your name:      ")
+mark = int(input("Type your mark:   "))
+
+if (mark > 43): 
+    print("Congratulations, you've passed your test.")
+else: 
+     print("Failed")
