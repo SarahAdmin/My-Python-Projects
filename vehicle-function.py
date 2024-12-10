50cb81f2b31a598adb3138814e38d3f22d8d69d5
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-10 12:38:07 +0000

    Create vehicle-function.py

---
+++ vehicle-function.py
@@ -0,0 +1,5 @@
+def myFunctionTwo(vehicle): 
+  for driveX in vehicle: 
+     print(driveX) 
+cars = ['Ford','Toyota','Volkswagen','Mini','Vauxhall','Hyndai','Honda','Nissan'] 
+print(myFunctionTwo(cars))
