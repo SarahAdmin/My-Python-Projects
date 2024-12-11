32a52417adf7207eb9847de6616d9036160d9d84
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-11 13:22:00 +0000

    Create my-car-class.py

---
+++ my-car-class.py
@@ -0,0 +1,14 @@
+class car: 
+  def __init__(self,brand,type,colour,noOfSeats): 
+     self.brand = brand
+     self.type = type 
+     self.colour = colour 
+     self.noOfSeats = noOfSeats 
+def carFunct(self): 
+  print('brand: '+self.brand)
+  print('type: '+self.type) 
+  print('colour: '+self.colour)
+  print('noOfSeats: '+self.noOfSeats) 
+par2 = car('Ford','Fiesta','Dark Blue','Five') 
+
+par2.carFunct()
