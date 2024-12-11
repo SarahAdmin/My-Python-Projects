c17a7be66f483e875ff5d83e59c2eb4b57f53cfa
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-11 13:28:05 +0000

    Update my-car-class.py

--- my-car-class.py
+++ my-car-class.py
@@ -4,11 +4,11 @@ class car:
      self.type = type 
      self.colour = colour 
      self.noOfSeats = noOfSeats 
-def carFunct(self): 
-  print('brand: '+self.brand)
-  print('type: '+self.type) 
-  print('colour: '+self.colour)
-  print('noOfSeats: '+self.noOfSeats) 
+  def carFunct(self): 
+      print('brand: '+ self.brand)
+      print('type: '+ self.type) 
+      print('colour: '+ self.colour)
+      print('noOfSeats: '+ self.noOfSeats) 
 par2 = car('Ford','Fiesta','Dark Blue','Five') 
 
 par2.carFunct()
