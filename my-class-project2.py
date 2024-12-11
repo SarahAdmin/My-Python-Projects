cebe6701089270ad648e8d7e5f0babe665a0accb
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-11 12:46:25 +0000

    Update my-class-project2.py

--- my-class-project2.py
+++ my-class-project2.py
@@ -1,7 +1,9 @@
 class MyStudent: 
   def __init__(self,forename,surname,subject): 
-  self.forename = forename 
-  self.surname = surname 
-  self.subject = subject 
-p2.MyStudent('Shaggy','Rogers','Digtal Technology') 
-print(p2)
+      self.forename = forename 
+      self.surname = surname 
+      self.subject = subject 
+p2 = MyStudent('Shaggy','Rogers','Digtal Technology') 
+print(p2.forename)
+print(p2.surname)
+print(p2.subject)
