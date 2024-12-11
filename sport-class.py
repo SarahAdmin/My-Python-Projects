cafcf1e0063df382c5a01d4fdec3842e4559b47b
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-11 12:51:23 +0000

    Create sport-class.py

---
+++ sport-class.py
@@ -0,0 +1,9 @@
+class SportBall: 
+  def __init__(self,type,colour,size): 
+      self.type = type 
+      self.colour = colour 
+      self.size = size  
+paramOne = SportBall('Netball','Pink','2.3cm') 
+print(paramOne.type)
+print(paramOne.colour)
+print(paramOne.size)
