1a6c015240238b0645c7e891ba03bd8100bb0cb4
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-11 12:40:17 +0000

    Create my-class-project2.py

---
+++ my-class-project2.py
@@ -0,0 +1,7 @@
+class MyStudent: 
+  def __init__(self,forename,surname,subject): 
+  self.forename = forename 
+  self.surname = surname 
+  self.subject = subject 
+p2.MyStudent('Shaggy','Rogers','Digtal Technology') 
+print(p2)
