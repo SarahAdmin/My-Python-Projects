9a86b2af9c9676dcb11a1f30200e7049dfbe66f6
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-17 20:41:06 +0000

    Update RollADice.py

--- RollADice.py
+++ RollADice.py
@@ -1,4 +1,26 @@
 import random 
 
 print("Roll the Dice") 
-print(random.randrange(1,6))
+dice = random.randrange(1,6)
+
+if dice == 1: 
+  print(dice)
+  print("You rolled Number 1")
+elif dice == 2: 
+  print(dice)
+  print("You rolled Number 2") 
+elif dice == 3: 
+  print(dice) 
+  print("You rolled Number 3")
+elif dice == 4: 
+  print(dice) 
+  print("You rolled Number 4")
+elif dice == 5: 
+  print(dice)
+  print("You rolled Number 5")
+elif dice == 6: 
+  print(dice) 
+  print("You rolled Number 6") 
+else: 
+  print("No Number")
+
