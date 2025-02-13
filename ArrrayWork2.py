a9aee8f82b1b2226f511623e2a7348d412b42743
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-02-13 20:55:57 +0000

    Create ArrrayWork2.py

---
+++ ArrrayWork2.py
@@ -0,0 +1,5 @@
+drinks = ['Lemonade','Cola','Milkshake','Apple Juice','Orange Juice']
+drinks.append('Pineapple Juice')
+drinks.insert(2, 'Strawberry Milkshake')
+drinks[3] = 'Vanilla Milkshake'
+print(drinks)
